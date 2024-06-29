10be7c45fb7b929137f981a4c4157e9adf56f58d
Dmitrii Novikov <kromez12@gmail.com>
2024-06-29 20:13:57 +0200

    add tree example

---
+++ tree.py
@@ -0,0 +1,50 @@
+def tree(root_label, branches=[]):
+    for branch in branches:
+        assert is_tree(branch), "branches must be trees"
+    return [root_label] + list(branches)
+
+
+def label(tree):
+    return tree[0]
+
+
+def branches(tree):
+    return tree[1:]
+
+
+def is_tree(tree):
+    if type(tree) != list or len(tree) < 1:
+        return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+
+def is_leaf(tree):
+    return not branches(tree)
+
+
+def partition_tree(n, m):
+    if n == 0:
+        return tree(True)
+    elif n < 0 or m == 0:
+        return tree(False)
+    else:
+        left = partition_tree(n - m, m)
+        right = partition_tree(n, m - 1)
+        return tree(m, [left, right])
+
+
+def print_parts(tree, partition=[]):
+    if is_leaf(tree):
+        if label(tree):
+            print(" + ".join(partition))
+    else:
+        left, right = branches(tree)
+        m = str(label(tree))
+        print_parts(left, partition + [m])
+        print_parts(right, partition)
+
+
+print_parts(partition_tree(6, 4))
