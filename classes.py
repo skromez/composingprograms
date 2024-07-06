3df313b1c62c90ed42e0e6a2ad61e2f8eb3720d3
Dmitrii Novikov <kromez12@gmail.com>
2024-07-06 12:44:55 +0200

    boring classes examples

---
+++ classes.py
@@ -0,0 +1,17 @@
+class Account:
+    def __init__(self, account_holder):
+        self.balance = 0
+        self.holder = account_holder
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return "Insufficient funds"
+        self.balance -= amount
+        return self.balance
+
+
+a = Account("Dima")
